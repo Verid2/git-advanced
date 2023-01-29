50cc3810c9b2267f11ec57dda58e69e21e111012
vkig <pakozdi.gergo@gmail.com>
2023-01-29 13:26:22 +0100

    c1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+namespace GreeterApp
+{
+    public class Greeter
+    {
+        public static void Main(string[] args)
+        {
+            cheer();
+        }
+        public static void cheer()
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
