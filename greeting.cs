4ae14099f66cec0f9fef9af3e308f0c60b32bcad
unknown <muteddnb22@gmail.com>
2023-01-29 13:21:49 +0100

    a1

---
+++ greeting.cs
@@ -0,0 +1,13 @@
+using System;
+class FunctionGreeting
+{
+    public static void Main(string[] args)
+    {
+        Greeting();
+    }
+ 
+    public static void Greeting()
+    {
+        Console.WriteLine("Hello");
+    }
+}
